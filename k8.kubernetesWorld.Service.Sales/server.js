b43098af25b9fe7e83dcad47966525fd19974add
Asif Khan <askha@microsoft.com>
2020-05-12 23:09:43 +1000

    Node app added staff service

--- k8.kubernetesWorld.Service.Sales/server.js
+++ k8.kubernetesWorld.Service.Sales/server.js
@@ -1,8 +1,31 @@
 'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n' + res.connection.localPort);
-}).listen(port);
+var express = require('express'); // Web Framework
+var app = express();
+var sql = require('mssql'); // MS Sql Server client
+
+// Connection string parameters.
+var sqlConfig = {
+    user: 'sa',
+    password: 'Redhat0!',
+    server: 'localhost',
+    database: 'staffDB'
+}
+
+// Start server and listen on http://localhost:8085/
+var server = app.listen(8085, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("app listening at http://%s:%s", host, port)
+});
+
+app.get('/staff', function (req, res) {
+    sql.connect(sqlConfig, function () {
+        var request = new sql.Request();
+        request.query('Select * from Staff', function (err, recordset) {
+            if (err) console.log(err);
+            res.end(JSON.stringify(recordset)); // Result in JSON format
+        });
+    });
+})
