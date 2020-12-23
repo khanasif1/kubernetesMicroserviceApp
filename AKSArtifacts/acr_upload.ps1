524e8b47c7201621a2dc9d03a8c0762050475122
khanasif1 <khanasif1@gmail.com>
2020-12-23 12:56:09 +1100

    Update files to define variable insted of hardcode

--- AKSArtifacts/acr_upload.ps1
+++ AKSArtifacts/acr_upload.ps1
@@ -1,48 +1,49 @@
 <#*********************
     Login 
 *********************#>
+$containerRegistryName = "msdemoregistry"
 az login
-az acr login --name msdemoregistry 
-az acr repository list --name msdemoregistry --output table
+az acr login --name  
+az acr repository list --name $containerRegistryName --output table
 docker images -a
 #docker rmi 3a48e3ebf9ac -f
 <#*********************
 Upload Product Image
 *********************#>
 #Tag images
-docker tag k8_product:rc1  msdemoregistry.azurecr.io/k8_product:1.00
+docker tag k8_product:rc1  $containerRegistryName.azurecr.io/k8_product:1.00
 #******Test Tag Image******
-#docker run -d -p 8082:80  --name productservice msdemoregistry.azurecr.io/k8_product:1.0
+#docker run -d -p 8082:80  --name productservice $containerRegistryName.azurecr.io/k8_product:1.0
 #Push to ACR
-docker push msdemoregistry.azurecr.io/k8_product:1.00
+docker push $containerRegistryName.azurecr.io/k8_product:1.00
 
 <#*********************
 Upload Staff Image
 *********************#>
 #Tag images
-docker tag k8_staff:rc1  msdemoregistry.azurecr.io/k8_staff:1.00
+docker tag k8_staff:rc1  $containerRegistryName.azurecr.io/k8_staff:1.00
 #******Test Tag Image******
-#docker run -d -p 8083:80  --name staffservice msdemoregistry.azurecr.io/k8_staff:1.0
+#docker run -d -p 8083:80  --name staffservice $containerRegistryName.azurecr.io/k8_staff:1.0
 #Push to ACR
-docker push msdemoregistry.azurecr.io/k8_staff:1.00
+docker push $containerRegistryName.azurecr.io/k8_staff:1.00
 
 <#*********************
 Upload Sales Image
 *********************#>
 #Tag images
-docker tag k8_sales:rc1  msdemoregistry.azurecr.io/k8_sales:1.00
+docker tag k8_sales:rc1  $containerRegistryName.azurecr.io/k8_sales:1.00
 #******Test Tag Image******
-#docker run -d -p 8085:80  --name saleservice msdemoregistry.azurecr.io/k8_sales:1.0
+#docker run -d -p 8085:80  --name saleservice $containerRegistryName.azurecr.io/k8_sales:1.0
 #Push to ACR
-docker push msdemoregistry.azurecr.io/k8_sales:1.00
+docker push $containerRegistryName.azurecr.io/k8_sales:1.00
 
 <#*********************
 Upload Web Image
 *********************#>
 #Tag images
-docker tag k8_web:rc1  msdemoregistry.azurecr.io/k8_web:1.00
+docker tag k8_web:rc1  $containerRegistryName.azurecr.io/k8_web:1.00
 <#******Test Tag Image******#>
-#docker run -d -p 8080:80  --name webservice msdemoregistry.azurecr.io/k8_web:1.0
+#docker run -d -p 8080:80  --name webservice $containerRegistryName.azurecr.io/k8_web:1.0
 #Push to ACR
-docker push msdemoregistry.azurecr.io/k8_web:1.00
+docker push $containerRegistryName.azurecr.io/k8_web:1.00
 
